3696d33ca3987116a3f7d7c147ae72ac9df10499
Stefan Fussenegger <stf+git@molindo.at>
2015-11-11 19:15:54 +0100

    added ecs-base ami

---
+++ ecs-base/README.md
@@ -0,0 +1,4 @@
+- built from [Amazon ECS AMI](http://docs.aws.amazon.com/AmazonECS/latest/developerguide/launch_container_instance.html)
+- available as `molindo-ecs-base-{{timestamp}}`
+- available in `us-east-1`, `us-west-2`, `eu-west-1`, `eu-central-1`
+- built using [Atlas](https://atlas.hashicorp.com/molindo/build-configurations/ecs-base)
---
+++ ecs-base/build.sh
@@ -0,0 +1,65 @@
+#!/bin/bash -e
+
+yum install -y curl aws-cli
+
+# replace docker TODO make version check dynamic
+docker version
+version="1.7.1"
+newVersion="1.9.0"
+if [ `echo $version | cut -d. -f1` -le 1 -a `echo $version | cut -d. -f2` -le 8 ]; then
+    curl "https://get.docker.com/builds/Linux/x86_64/docker-$newVersion" > /tmp/docker
+    chmod +x /tmp/docker
+    echo "replacing docker $version with $newVersion"
+    exec=`which docker`
+    mv $exec $exec-dist
+    mv /tmp/docker $exec
+fi
+docker version
+
+cat >> /etc/ecs/ecs.config <<'ECS_CONFIG'
+ECS_AVAILABLE_LOGGING_DRIVERS=["json-file","syslog","awslogs"]
+ECS_CONFIG
+
+# script to update ec2 profile
+cat > /usr/local/bin/update-ec2-env.sh <<'EC2ENV'
+#!/bin/bash
+
+region=$(curl http://169.254.169.254/latest/meta-data/placement/availability-zone | egrep -o '^[a-z]+-[a-z]+-[0-9]')
+instance=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+
+cat > /etc/profile.d/01-ec2-env.sh <<ENV
+# this file is auto-generated, don't edit!
+
+# globals
+export EC2_REGION=$region
+export EC2_INSTANCE_ID=$instance
+
+# aws cli
+export AWS_DEFAULT_REGION=$region
+
+ENV
+EC2ENV
+chmod +x /usr/local/bin/update-ec2-env.sh
+
+# profile placeholder
+echo '# ec2-env not updated yet!' > /etc/profile.d/01-ec2-env.sh
+
+# systemd unit to update ec2 profile on startup
+cat > /etc/init/ec2-env.conf <<'UPSTART'
+description "Update EC2 profile (/etc/profile.d/01-ec2-env.sh)"
+
+start on started elastic-network-interfaces
+
+script
+    /usr/local/bin/update-ec2-env.sh
+end script
+UPSTART
+
+cat > /usr/local/bin/ec2-tag.sh <<'EC2TAG'
+#!/bin/bash
+
+. /etc/profile.d/01-ec2-env.sh
+
+aws ec2 describe-tags --filters Name=resource-id,Values=$EC2_INSTANCE_ID Name=key,Values=${1-Name} --query 'Tags[0].Value' --output text
+EC2TAG
+chmod +x /usr/local/bin/ec2-tag.sh
